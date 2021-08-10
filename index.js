4e7f85e3c7f884b723d94560f3cd1177be264d9e
mxydl2009 <mxydl2009@163.com>
2021-08-10 10:52:47 +0800

    update index.js exports

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import createStore from './src/createStore'
 import applyMiddleware, { compose } from './src/applyMiddleware'
 import bindActionCreators from './src/bindActionCreators'
+import combineReducer from './src/combineReducer'
 export {
   createStore,
+  combineReducer,
   applyMiddleware,
   bindActionCreators,
   compose
