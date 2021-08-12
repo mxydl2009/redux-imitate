dabf801fbe89ec4e7d42dcceff185aaef622c473
mxydl2009 <mxydl2009@163.com>
2021-08-12 10:18:46 +0800

    add comments

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -10,6 +10,21 @@
  *     }
  *   }
  * }
+ * 如redux-thunk中间件的源码
+ * function createThunkMiddleware(extraArgument) {
+      return ({ dispatch, getState }) => (next) => (action) => {
+        if (typeof action === 'function') {
+          return action(dispatch, getState, extraArgument);
+        }
+
+        return next(action);
+      };
+    }
+
+    const thunk = createThunkMiddleware();
+    thunk.withExtraArgument = createThunkMiddleware;
+
+    export default thunk;
  */
 export default function applyMiddleware(...midllewares) {
   // 返回一个函数作为enhancer
